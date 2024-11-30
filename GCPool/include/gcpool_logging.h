8710fc407d083a2e53096de8cc154af6b3c2e308
INA <harbor@kaist.ac.kr>
2024-11-30 15:18:43 +0900

    new update

--- GCPool/include/gcpool_logging.h
+++ GCPool/include/gcpool_logging.h
@@ -13,29 +13,29 @@ extern FILE* gcpoolInfoFile;
 
 void gcpoolInfoLog(const char* filefunc, int line, const char* fmt, ...);
 
-#define GCPOOL_INFO(...) \
-    do { \
-        gcpoolInfoLog(__func__, __LINE__, __VA_ARGS__); \
+#define GCPOOL_INFO(...) 
+    do { 
+        gcpoolInfoLog(__func__, __LINE__, __VA_ARGS__); 
     } while (0)
 
 #define gettid() (pid_t)syscall(SYS_gettid)
 
-#define gtrace() { \
-    void* traces[32]; \
-    int size = backtrace(traces, 32); \
-    char** msgs = backtrace_symbols(traces, size); \
-    if (msgs == NULL) { \
-        exit(EXIT_FAILURE); \
-    } \
-    printf("------------------\n"); \
-    for (int i = 0; i < size; i++) { \
-        printf("[bt] #%d %s symbol:%p \n", i, msgs[i], traces[i]); \
-        fflush(stdout); \
-    } \
-    printf("------------------\n"); \
-    free(msgs); \
+#define gtrace() { 
+    void* traces[32]; 
+    int size = backtrace(traces, 32); 
+    char** msgs = backtrace_symbols(traces, size); 
+    if (msgs == NULL) { 
+        exit(EXIT_FAILURE); 
+    } 
+    printf("------------------n"); 
+    for (int i = 0; i < size; i++) { 
+        printf("[bt] #%d %s symbol:%p n", i, msgs[i], traces[i]); 
+        fflush(stdout); 
+    } 
+    printf("------------------n"); 
+    free(msgs); 
 }
 
-#define LOGE(format, ...) fprintf(stdout, "L%d:" format "\n", __LINE__, ##__VA_ARGS__); fflush(stdout)
+#define LOGE(format, ...) fprintf(stdout, "L%d:" format "n", __LINE__, ##__VA_ARGS__); fflush(stdout)
 #define ASSERT(cond, ...) { if (!(cond)) { LOGE(__VA_ARGS__); assert(0); } }
 #define WARN(cond, ...) { if (!(cond)) { LOGE(__VA_ARGS__); } }
--- GCPool/include/utils.h
+++ GCPool/include/utils.h
@@ -13,25 +13,25 @@ void gcpoolInfoInit();
 
 void getHostName(char* hostname, int maxlen, const char delim);
 
-#define DRV_CALL(call) \
-    { \
-        CUresult result = (call); \
-        if (CUDA_SUCCESS != result) { \
-            const char* errMsg; cuGetErrorString(result, &errMsg); \
-            ASSERT(0, "Error when exec " #call " %s-%d code:%d err:%s", __FUNCTION__, __LINE__, result, errMsg); \
-        } \
+#define DRV_CALL(call) 
+    { 
+        CUresult result = (call); 
+        if (CUDA_SUCCESS != result) { 
+            const char* errMsg; cuGetErrorString(result, &errMsg); 
+            ASSERT(0, "Error when exec " #call " %s-%d code:%d err:%s", __FUNCTION__, __LINE__, result, errMsg); 
+        } 
     }
 
-#define DRV_CALL_RET(call, status_val) \
-    { \
-        if (CUDA_SUCCESS == status_val) { \
-            CUresult result = (call); \
-            if (CUDA_SUCCESS != result) { \
-                const char* errMsg; cuGetErrorString(result, &errMsg); \
-                WARN(0, "Error when exec " #call " %s-%d code:%d err:%s", __FUNCTION__, __LINE__, result, errMsg); \
-            } \
-            status_val = result; \
-        } \
+#define DRV_CALL_RET(call, status_val) 
+    { 
+        if (CUDA_SUCCESS == status_val) { 
+            CUresult result = (call); 
+            if (CUDA_SUCCESS != result) { 
+                const char* errMsg; cuGetErrorString(result, &errMsg); 
+                WARN(0, "Error when exec " #call " %s-%d code:%d err:%s", __FUNCTION__, __LINE__, result, errMsg); 
+            } 
+            status_val = result; 
+        } 
     }
 
 static constexpr size_t granularitySize = 2097152;
